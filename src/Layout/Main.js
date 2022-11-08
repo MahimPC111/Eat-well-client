339870e28091163d9f1b67aeadc714323277aac9
MahimPC111 <mahimpc111@gmail.com>
2022-11-08 10:23:58 +0600

    setup  commit

---
+++ src/Layout/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from '../Components/Shared/Header/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            {/* <Outlet></Outlet> */}
+        </div>
+    );
+};
+
+export default Main;
