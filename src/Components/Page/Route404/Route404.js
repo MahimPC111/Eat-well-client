942b87836a22651edef8cdad47c62cf3387f1ef1
MahimPC111 <mahimpc111@gmail.com>
2022-11-10 22:10:24 +0600

    404 route committed

---
+++ src/Components/Page/Route404/Route404.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Route404.css'
+
+const Route404 = () => {
+    return (
+        <div className='bg404'>
+            <h3 className='text-center pt-5'>Invalid request!</h3>
+        </div>
+    );
+};
+
+export default Route404;
